4e1269a8f2b0367dfe69d062e151f650d37c0d30
EC2 Default User <ec2-user@ip-172-31-44-135.ap-southeast-1.compute.internal>
2019-03-09 10:56:59 +0000

    修正

--- main.js
+++ main.js
@@ -13,18 +13,6 @@ var app = new Vue({
     message:'',
     keyword:'', //この記述がないと定義されていませんとエラーがでるようだ
   },
-  watch: {
-      keyword: function(newKeyword, oldKeyword){
-        // lodash.debounceを利用してAPI呼び出しの負荷軽減
-        this.message = '入力が終わるのを待ってます･･･';
-        this.debouncedGetAnswer();
-      }
-    },
-    created: function() {
-      // lodash.debounce によって、1秒間操作が無くなった時点でAPI呼び出し実施
-      // キー入力の度にAPIを呼び出すような負荷をかけないため
-      this.debouncedGetAnswer = _.debounce(this.getAnswer, 1000);
-    },
   methods:{
     getAnswer: function(){
         // キーワードが空の場合はメッセージと検索結果を空にして処理終了
