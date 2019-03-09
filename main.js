134e4fccfef32ccd5c50324866dbd114f19a6a5a
EC2 Default User <ec2-user@ip-172-31-44-135.ap-southeast-1.compute.internal>
2019-03-09 07:09:20 +0000

    updated button

--- main.js
+++ main.js
@@ -1,43 +1,25 @@
-var app = new Vue({
+
+const GoogleBooksApi = "'https://www.googleapis.com/books/v1/volumes?q=search";
+
+function buildUrl(url){
+  return GoogleBooksApi + url
+}
+
+const vm = new Vue({
   el: '#app',
   data: {
-    items: []
+    results: []
   },
-  watch:{ //watchの意味を理解する
-    keyword: function(newKeyword,oldKeyword){
-    // loadash.debounceを利用してAPI呼び出しの負荷軽減  
-      this.message="入力が終わるのを待っています";
-      this.debouncedGetAnswer();
-    }
-  },
-  created: function(){
-    //loadash.debounceによって、１秒間操作がなくなった時点でAPI呼び出し実施
-    //キー入力のたびにAPIを呼び出すような負荷をかけないため
-    this.debouncedGetAnswer = _.debounce(this.getAnswer,1000);
+  mounted(){
+    this.getItems('home')
   },
   methods:{
-    getAnswer: function(){
-      //キーワードが空の場合は、メッセージと検索結果を空にして処理終了
-      if(this.keyword===''){
-        this.items = null;
-        this.message = '';
-        return;
-      }
+    getItems(keyword){
+      let url = buildUrl(keyword); //なぜletを使うのか？
+      axios.get(url).then((response) => {
+        this.results = response.data.results;
+      }).catch(error => {console.log(error);});
       
-      this.message = '検索しています・・・';
-      var vm = this;
-      var params = {page:1, per_page: 20, query: this.keyword };
-      axios.get('https://www.googleapis.com/books/v1/volumes?q=search' + {params})
-      .then(function(response){
-        console.log(response);
-        vm.items = response.data;
-      })
-      .catch(function(error){
-        vm.message = 'Error!' + error;
-      })
-      .catch(function(){
-        vm.message='';
-      })
     }
     
   }
