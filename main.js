f0f9283eeafd81686a4ac02b0be05b51661a18e6
EC2 Default User <ec2-user@ip-172-31-44-135.ap-southeast-1.compute.internal>
2019-03-09 10:20:48 +0000

    updated

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+import Vue from 'vue'
 import axios from 'axios'; //呼び出し方あっているか？
 const GoogleBooksApi = "'https://www.googleapis.com/books/v1/volumes?q=search";
 
@@ -6,27 +7,41 @@ function buildUrl(url){
   return GoogleBooksApi + url
 }
 
-const vm = new Vue({
+var app = new Vue({
   el: '#app',
   data: {
-    results: [],
+    items: null,
+    message:'',
     keyword:'', //この記述がないと定義されていませんとエラーがでるようだ
   },
-  mounted(){
-    this.getItems(this.keyword);
-  },
+  watch: {
+      keyword: function(newKeyword, oldKeyword){
+        // lodash.debounceを利用してAPI呼び出しの負荷軽減
+        this.message = '入力が終わるのを待ってます･･･';
+        this.debouncedGetAnswer();
+      }
+    },
+    created: function() {
+      // lodash.debounce によって、1秒間操作が無くなった時点でAPI呼び出し実施
+      // キー入力の度にAPIを呼び出すような負荷をかけないため
+      this.debouncedGetAnswer = _.debounce(this.getAnswer, 1000);
+    },
   methods:{
-    getItems(keyword){
-      let url = buildUrl(keyword); //なぜletを使うのか？
+    getAnswer: function(){
+        // キーワードが空の場合はメッセージと検索結果を空にして処理終了
+        if( this.keyword === ''){
+          this.items = null;
+          this.message = '';
+          return;
+        }
+ 
+        this.message = 'loading...';
+        var vm = this;
+    var params = {query: this.keyword };
+    let url = buildUrl(params); //なぜletを使うのか？
       axios.get(url).then((response) => {
-        this.results = response.data.results;
+        this.items = response.data.results;
       }).catch(error => {console.log(error);});
-      
     }
   },
-  computed:{ //computedってなんだ？
-    processedItems(){
-      let items = this.results;
-    }
-  }
 });
