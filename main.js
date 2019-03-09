16f97482e7cb11a055775ae56a0a5ddf587a4d59
EC2 Default User <ec2-user@ip-172-31-44-135.ap-southeast-1.compute.internal>
2019-03-09 07:42:11 +0000

    install axios

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 
 const GoogleBooksApi = "'https://www.googleapis.com/books/v1/volumes?q=search";
+const axios = require('axios'); //npm install axios --saveでインストール後、記述
 
 function buildUrl(url){
   return GoogleBooksApi + url
@@ -9,7 +10,7 @@ const vm = new Vue({
   el: '#app',
   data: {
     results: [],
-    keyword:'',
+    keyword:'', //この記述がないと定義されていませんとエラーがでるようだ
   },
   mounted(){
     this.getItems(this.keyword);
