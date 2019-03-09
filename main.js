42ae98913adaab132849857d624400d96f1eb31e
EC2 Default User <ec2-user@ip-172-31-44-135.ap-southeast-1.compute.internal>
2019-03-09 03:07:37 +0000

    add search form

--- main.js
+++ main.js
@@ -2,11 +2,10 @@
 const vm = new Vue({
   el: '#app',
   data: {
-    results: [
-      {title: "the very first post", abstract: "lorem ipsum some test dimpsum"},
-      {title: "and then there was the second", abstract: "lorem ipsum some test dimsum"},
-      {title: "third time's a charm", abstract: "lorem ipsum some test dimsum"},
-      {title: "four the last time", abstract: "lorem ipsum some test dimsum"}
-    ]
+    results: []
+  },
+  mounted(){
+    axios.get("hogehoge")
+    .then(response => {this.results = response.data.results})
   }
 });
