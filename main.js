2e30bb1f2d6d1d1370060806cd67edd1771b1a4c
EC2 Default User <ec2-user@ip-172-31-44-135.ap-southeast-1.compute.internal>
2019-03-10 01:31:44 +0000

    修正

--- main.js
+++ main.js
@@ -1,53 +1,27 @@
 
 const GoogleBooksApi = "https://www.googleapis.com/books/v1/volumes?q=search";
-import _ from 'lodash'
+
 
 function buildUrl(url){
   return GoogleBooksApi + url
 }
 
-var app = new Vue({
+const vm = new Vue({
   el: '#app',
   data: {
-    items: null,
-    message:'',
-    keyword:'', //この記述がないと定義されていませんとエラーがでるようだ
+    items: [],
+    keyword:'',//この記述がないと定義されていませんとエラーがでるようだ
+    title:'',
+    author:'',
   },
-  watch: {
-      keyword: function(newKeyword, oldKeyword){
-        // lodash.debounceを利用してAPI呼び出しの負荷軽減
-        this.message = '入力が終わるのを待ってます･･･';
-        this.debouncedGetAnswer();
-      }
-    },
-    created: function() {
-      // lodash.debounce によって、1秒間操作が無くなった時点でAPI呼び出し実施
-      // キー入力の度にAPIを呼び出すような負荷をかけないため
-      this.debouncedGetAnswer = _.debounce(this.getAnswer, 1000);
-    },
   methods:{
-    getAnswer: function(){
-        // キーワードが空の場合はメッセージと検索結果を空にして処理終了
-        if( this.keyword === ''){
-          this.items = null;
-          this.message = '';
-          return;
-        }
- 
-        this.message = 'loading...';
-        var vm = this;
-    var params = {query: this.keyword };
-    let url = buildUrl(params); //なぜletを使うのか？
-      axios.get(url).then(function(response){
-            console.log(response);
-            vm.items = response.data;
-          })
-          .catch(function(error){
-            vm.message = 'Error!' + error;
-          })
-          .finally(function(){
-            vm.message = '';
-          })
+    getItems(keyword){
+     let url = buildUrl(keyword);
+     axios.get(url).then((response) =>
+     {this.items = response.data.items;
+     }).catch((error) => {
+       console.log(error);
+     });
     }
   },
 });
