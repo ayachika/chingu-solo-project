2fc592b621c62d8becf526e38c90acefae63d51d
EC2 Default User <ec2-user@ip-172-31-44-135.ap-southeast-1.compute.internal>
2019-03-11 14:18:59 +0000

    修正

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ const vm = new Vue({
     title:'',
     author:'',
   },
-  mounted:{
+  methods:{
     getItems(keyword){
      const url = buildUrl(keyword);
      return axios.get(url).then(response =>
