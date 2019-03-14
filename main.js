5d2bf45b77d24f87145aa557d1abfda8704cdb0d
EC2 Default User <ec2-user@ip-172-31-44-135.ap-southeast-1.compute.internal>
2019-03-14 22:43:03 +0000

    修正

--- main.js
+++ main.js
@@ -2,13 +2,14 @@ const vm = new Vue({
   el: '#app',
   data() {
     return{
-      query:'Ayaka',
+      query:'',
       items:[]
       }
     },
   methods:{
     getResult(query){
       axios.get("https://www.googleapis.com/books/v1/volumes?q=search" + query).then(response => {console.log(response.data);
+        this.items = response.data;
       });
       }
     }
