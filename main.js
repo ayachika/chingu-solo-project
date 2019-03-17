abf7e7994ebbc6e54d076f7a0b431fd646d91230
EC2 Default User <ec2-user@ip-172-31-44-135.ap-southeast-1.compute.internal>
2019-03-17 06:54:03 +0000

    修正

--- main.js
+++ main.js
@@ -4,16 +4,13 @@ const vm = new Vue({
     return{
       query:'',
       items:[],
-      loading: false
       }
     },
   methods:{
     getResult(query){
       axios.get("https://www.googleapis.com/books/v1/volumes?q=search" + query).then(response => {
-        this.loading = true;
-        this.items = response.data; //返されたデータをitemsに格納する
-        this.loading = false; //初期状態ではloadingプロパティをtrueにしておき、axiosでのAPI通信が終わった時点でfalseにする
-      });
+        this.items = response.data;
+        });
       }
     }
 });
